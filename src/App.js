418ba9a5a66b8f34a0dac8292bdb6cf64f366f54
suman <sumanoffice520@gmail.com>
2024-03-15 21:42:24 +0545

    readdy to commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to the site hosted from docker.
         </p>
         <a
           className="App-link"
